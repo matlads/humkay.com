e03027c7aabe03467e4c98374502a38668236719
Martin Atukunda <matlads@gmail.com>
2018-08-05 16:20:25 +0300

    Align the base_url port with the port in the Makefile

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://0.0.0.0:8001/';
+$config['base_url'] = 'http://0.0.0.0:8000/';
 
 /*
 |--------------------------------------------------------------------------
