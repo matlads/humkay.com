8474677aceee46d21d95f46741f2020ec3579ae4
Martin Atukunda <matlads@gmail.com>
2018-08-05 22:47:20 +0300

    Add events.sql

---
+++ events.sql
@@ -0,0 +1,11 @@
+INSERT INTO events(date, name, location)
+VALUES
+ ('2018-06-29', 'Red Sofa Session', 'Golf Course Hotel, Kampala, Uganda'),
+ ('2018-07-14', 'Annual Worship Ministers Summit', 'Deliverance Church Makerere, Kampala, Uganda'),
+ ('2018-07-15', 'Follow up service to the AWMS', 'Eden Revival Church, Wandegeya, Kampala, Uganda'),
+ ('2018-08-08', 'Christafari Concert', 'The Worship House, Kampala, Uganda'),
+ ('2018-08-24', 'Worship Celebration', 'Invite Only'),
+ ('2018-08-26', '2 day Overflow Africa Worship Conference', 'CLA, Kigali, Rwanda'),
+ ('2018-08-31', 'Christo Centric Worship Event', 'Kigali, Rwanda'),
+ ('2018-10-07', 'Kampala City Festival 2018', 'Kampala, Uganda')
+;
